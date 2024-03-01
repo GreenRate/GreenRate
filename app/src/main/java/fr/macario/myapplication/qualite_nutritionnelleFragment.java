320616703c8ff0fb7976d50bc7766c8f10a5d9b6
BuildTools <unconfigured@null.spigotmc.org>
2024-03-01 18:14:07 +0100

    mise en place des choix pour le fichier qualite_nutritionnelleFragment.java

--- app/src/main/java/fr/macario/myapplication/qualite_nutritionnelleFragment.java
+++ app/src/main/java/fr/macario/myapplication/qualite_nutritionnelleFragment.java
@@ -1,17 +1,14 @@
 package fr.macario.myapplication;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.text.Html;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import fr.macario.myapplication.databinding.FragmentPrefAlimentairesBinding;
 import fr.macario.myapplication.databinding.FragmentQualiteNutritionnelleBinding;
 
 
@@ -59,6 +56,160 @@ public class qualite_nutritionnelleFragment extends Fragment {
                 binding.pasimportantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
                 binding.importantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
                 binding.obligatoirequaliteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantqualiteenutritionnelle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bonnequal = 1;
+
+                binding.pasimportantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoirequaliteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoirequaliteenutritionnelle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bonnequal = 2;
+
+                binding.pasimportantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantqualiteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirequaliteenutritionnelle.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantmatiereqrasse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faibleQuantMatiereGrasse = 0;
+
+                binding.pasimportantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirematiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantmatiereqrasse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faibleQuantMatiereGrasse = 1;
+
+                binding.pasimportantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoirematiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoirematiereqrasse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faibleQuantMatiereGrasse = 2;
+
+                binding.pasimportantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantmatiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirematiereqrasse.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantsel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSel = 0;
+
+                binding.pasimportantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiresel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantsel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSel = 1;
+
+                binding.pasimportantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoiresel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoiresel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSel = 2;
+
+                binding.pasimportantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantsel.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiresel.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantsucre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSucre = 0;
+
+                binding.pasimportantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiresucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantsucre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSucre = 1;
+
+                binding.pasimportantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoiresucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoiresucre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantSucre = 2;
+
+                binding.pasimportantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantsucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiresucre.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantgras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantAcide = 0;
+
+                binding.pasimportantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiregras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantgras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantAcide = 1;
+
+                binding.pasimportantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoiregras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoiregras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantAcide = 2;
+
+                binding.pasimportantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantgras.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiregras.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
             }
         });
     }
