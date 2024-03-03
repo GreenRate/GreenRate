e98d542b4dbeaf7364cef3403135b8b4550aa243
BuildTools <unconfigured@null.spigotmc.org>
2024-03-03 10:11:54 +0100

    mise en place des bouton pour les ingrédients

--- app/src/main/java/fr/macario/myapplication/ingredientFragment.java
+++ app/src/main/java/fr/macario/myapplication/ingredientFragment.java
@@ -18,6 +18,9 @@ public class ingredientFragment extends Fragment {
     private FragmentIngredientBinding binding;
     public ingredientFragment() {}
 
+    public int vegan;
+    public int vegetarien;
+    public int gluten;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
@@ -40,6 +43,105 @@ public class ingredientFragment extends Fragment {
                 PrefAlimentairesFragment prefAlimentairesFragment = new PrefAlimentairesFragment();
                 fragmentTransaction.add(R.id.fragment_container_view, prefAlimentairesFragment);
                 fragmentTransaction.commit();
+            }
+        });
+
+        binding.pasimportantvegan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegan = 0;
+
+                binding.pasimportantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirevegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantvegan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegan = 1;
+
+                binding.pasimportantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoirevegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoirevegan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegan = 2;
+
+                binding.pasimportantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantvegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirevegan.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantvegetarien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegetarien = 0;
+
+                binding.pasimportantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirevegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantvegetarien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegetarien = 1;
+
+                binding.pasimportantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoirevegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoirevegetarien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vegetarien = 2;
+
+                binding.pasimportantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantvegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoirevegetarien.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+            }
+        });
+
+        binding.pasimportantgluten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gluten = 0;
+
+                binding.pasimportantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.importantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiregluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.importantgluten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gluten = 1;
+
+                binding.pasimportantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
+                binding.obligatoiregluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+            }
+        });
+
+        binding.obligatoiregluten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gluten = 2;
+
+                binding.pasimportantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.importantgluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Small);
+                binding.obligatoiregluten.setTextAppearance(getContext(), android.R.style.TextAppearance_Large);
             }
         });
     }
