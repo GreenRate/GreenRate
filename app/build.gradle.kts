c5ff3cf91cc4877cc0e5698e5172d71c5ce8e3ed
Admin <clgorau@stpbb.org>
2024-03-25 19:03:32 +0100

    1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "fr.macario.myapplication"
         minSdk = 19
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
