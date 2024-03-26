ceea3a0993f5f3559c71143c51b984706406812b
Admin <clgorau@stpbb.org>
2024-03-26 19:21:34 +0100

    suite nous contacter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
         minSdk = 19
         targetSdk = 34
         versionCode = 2
+
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -39,6 +40,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
